7ca0089441383dc27f0a0bcddedecf710e934ed5
PinRong1014 <113153001@gas.fgu.edu.tw>
2025-05-05 14:39:44 +0800

    重作下一步功能

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,15 +35,16 @@
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             this.listUndo = new System.Windows.Forms.ListBox();
             this.btnUndo = new System.Windows.Forms.Button();
+            this.btnRedo = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bntOpen
             // 
             this.bntOpen.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.bntOpen.Location = new System.Drawing.Point(11, 11);
-            this.bntOpen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bntOpen.Location = new System.Drawing.Point(15, 14);
+            this.bntOpen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.bntOpen.Name = "bntOpen";
-            this.bntOpen.Size = new System.Drawing.Size(119, 41);
+            this.bntOpen.Size = new System.Drawing.Size(159, 51);
             this.bntOpen.TabIndex = 0;
             this.bntOpen.Text = "開啟檔案";
             this.bntOpen.UseVisualStyleBackColor = true;
@@ -51,10 +52,10 @@
             // 
             // rtbText
             // 
-            this.rtbText.Location = new System.Drawing.Point(9, 54);
-            this.rtbText.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rtbText.Location = new System.Drawing.Point(12, 68);
+            this.rtbText.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rtbText.Name = "rtbText";
-            this.rtbText.Size = new System.Drawing.Size(627, 376);
+            this.rtbText.Size = new System.Drawing.Size(835, 469);
             this.rtbText.TabIndex = 2;
             this.rtbText.Text = "";
             this.rtbText.TextChanged += new System.EventHandler(this.rtbText_TextChanged);
@@ -62,10 +63,10 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnSave.Location = new System.Drawing.Point(134, 11);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSave.Location = new System.Drawing.Point(179, 14);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(119, 41);
+            this.btnSave.Size = new System.Drawing.Size(159, 51);
             this.btnSave.TabIndex = 3;
             this.btnSave.Text = "存檔";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -78,37 +79,50 @@
             // listUndo
             // 
             this.listUndo.FormattingEnabled = true;
-            this.listUndo.ItemHeight = 12;
-            this.listUndo.Location = new System.Drawing.Point(640, 54);
-            this.listUndo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listUndo.ItemHeight = 15;
+            this.listUndo.Location = new System.Drawing.Point(853, 68);
+            this.listUndo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.listUndo.Name = "listUndo";
-            this.listUndo.Size = new System.Drawing.Size(158, 376);
+            this.listUndo.Size = new System.Drawing.Size(209, 469);
             this.listUndo.TabIndex = 4;
             // 
             // btnUndo
             // 
             this.btnUndo.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.btnUndo.Location = new System.Drawing.Point(257, 11);
-            this.btnUndo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUndo.Location = new System.Drawing.Point(343, 14);
+            this.btnUndo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnUndo.Name = "btnUndo";
-            this.btnUndo.Size = new System.Drawing.Size(119, 41);
+            this.btnUndo.Size = new System.Drawing.Size(159, 51);
             this.btnUndo.TabIndex = 5;
             this.btnUndo.Text = "復原上一步";
             this.btnUndo.UseVisualStyleBackColor = true;
             this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
             // 
+            // btnRedo
+            // 
+            this.btnRedo.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnRedo.Location = new System.Drawing.Point(508, 11);
+            this.btnRedo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRedo.Name = "btnRedo";
+            this.btnRedo.Size = new System.Drawing.Size(159, 51);
+            this.btnRedo.TabIndex = 6;
+            this.btnRedo.Text = "重作下一步";
+            this.btnRedo.UseVisualStyleBackColor = true;
+            this.btnRedo.Click += new System.EventHandler(this.btnRedo_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ClientSize = new System.Drawing.Size(806, 438);
+            this.ClientSize = new System.Drawing.Size(1075, 548);
+            this.Controls.Add(this.btnRedo);
             this.Controls.Add(this.btnUndo);
             this.Controls.Add(this.listUndo);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.rtbText);
             this.Controls.Add(this.bntOpen);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -124,6 +138,7 @@
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
         private System.Windows.Forms.ListBox listUndo;
         private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnRedo;
     }
 }
 
